d3ef505e71ea2681cc5621f108361deb0438411a
bryangregory-hub <73159622+bryangregory-hub@users.noreply.github.com>
2021-12-23 12:22:08 +0800

    movement yes

--- Unity/Assets/Scripts/PlayerMovement.cs
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.OpenXR;
+using Unity.XR.CoreUtils;
 
 [System.Serializable]
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1;
     public XRNode inputSource;
-
-    private XRRig rig;
+    [System.Obsolete]
+    public XROrigin rig;
     private Vector2 inputAxis;
     private CharacterController character;
+    public Camera playerCamera;
 
     // Start is called before the first frame update
+    [System.Obsolete]
     void Start()
     {
         character = GetComponent<CharacterController>();
-        rig = GetComponent<XRRig>();
+        rig = GetComponent<XROrigin>();
     }
 
     // Update is called once per frame
@@ -29,10 +31,10 @@ public class PlayerMovement : MonoBehaviour
         device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
         
     }
-
+    [System.Obsolete]
     private void FixedUpdate()
     {
-        Quaternion headYaw = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
+        Quaternion headYaw = Quaternion.Euler(0, playerCamera.transform.eulerAngles.y, 0);  
         Vector3 direction =headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
 
         character.Move(direction * Time.fixedDeltaTime * speed);
