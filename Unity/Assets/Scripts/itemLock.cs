5464145117a4666d41582dc5966545536b0892cc
bryangregory-hub <73159622+bryangregory-hub@users.noreply.github.com>
2021-12-24 15:06:30 +0800

    starting to the part 1

--- Unity/Assets/Scripts/itemLock.cs
+++ Unity/Assets/Scripts/itemLock.cs
@@ -5,25 +5,40 @@ using System;
 
 public class itemLock : MonoBehaviour
 {
-    //if needed
-    public GameObject ItemCol;
-    
+    public enum Components { Screw, cpu , etc}
+
+    public Components Component;
+    [Header ("Other component")]
     public GameObject ItemAni;
 
     public GameObject ItemModel;
+    [Header ("Screw")]
+    //if needed
+    public GameObject ItemCol;
+
     //if needed
     public GameObject RotateScrew;
-    private void Start()
+
+    private bool screwTrue = false;
+     void Start()
     {
         
     }
-    private void Update()
+    void Update()
     {
-        
+        switch (Component)
+        {
+            case Components.Screw:
+                screwTrue = true;
+                break;
+            case Components.cpu:
+                print("hoho");
+                break;
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Screw")
+        if (collision.gameObject.tag == "Screw" && screwTrue == true)
         {
             ItemCol.GetComponent<ScrewInteract>().enabled=true;
             ItemAni.SetActive(true);
